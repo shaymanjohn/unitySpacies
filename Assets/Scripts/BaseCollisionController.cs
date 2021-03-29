d8421ffe9bde0ff707c6416a7b2b997b9f1af981
John Ward <johnmartinward@gmail.com>
2021-03-29 17:39:56 +0100

    refactored base handling

--- Assets/Scripts/BaseCollisionController.cs
+++ Assets/Scripts/BaseCollisionController.cs
@@ -3,9 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BaseCollisionController : MonoBehaviour {
+
+    private BaseController baseController;
+
+    void Start() {
+        GameObject baseManager = GameObject.Find("BaseManager");
+        if (baseManager != null) {
+            baseController = baseManager.GetComponent<BaseController>();
+        }
+    }
+
     void OnTriggerEnter2D(Collider2D other) {
         if (other.tag.Contains(GameManager.tagBomb)) {
-            GameManager.log("bomb hit base");
+            baseController.hasBombHitBase(gameObject, other.gameObject);
         }
     }
 }
--- Assets/Scripts/BaseController.cs
+++ Assets/Scripts/BaseController.cs
@@ -3,36 +3,67 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BaseController : MonoBehaviour {
-    public Texture2D baseTexture;
-    public SpriteRenderer base1Renderer;
-    public SpriteRenderer base2Renderer;
-    public SpriteRenderer base3Renderer;
-    public SpriteRenderer base4Renderer;
+    public GameObject basePrefab;
+
+    private Base[] bases = new Base[4];
 
     void Start() {
-        createBase(base1Renderer);
-        createBase(base2Renderer);
-        createBase(base3Renderer);
-        createBase(base4Renderer);
+        bases[0] = createBase(new Vector2(-4, -4), Color.white);
+        bases[1] = createBase(new Vector2(-1, -4), Color.blue);
+        bases[2] = createBase(new Vector2( 2, -4), Color.red);
+        bases[3] = createBase(new Vector2( 5, -4), Color.magenta);
     }
 
-    private void createBase(SpriteRenderer baseRenderer) {
-        Texture2D thisBase = new Texture2D(baseTexture.width, baseTexture.height);
-        thisBase.filterMode = FilterMode.Point;
+    private Base createBase(Vector2 position, Color colour) {
+        GameObject baseObj = Instantiate(basePrefab, position, new Quaternion(0, 0, 0, 0));
+
+        SpriteRenderer renderer = baseObj.GetComponent<SpriteRenderer>();
+        Texture2D baseTexture = renderer.sprite.texture;
+
+        Texture2D thisBaseTexture = new Texture2D(baseTexture.width, baseTexture.height);
+        thisBaseTexture.filterMode = FilterMode.Point;
+
+        bool[,] collisionGrid = new bool[baseTexture.width, baseTexture.height];
 
         for (int ix = 0; ix < baseTexture.width; ix++) {
             for (int iy = 0; iy < baseTexture.height; iy++) {
                 Color pixel = baseTexture.GetPixel(ix, iy);
-                thisBase.SetPixel(ix, iy, pixel);
+                if (pixel.a > 0) {
+                    thisBaseTexture.SetPixel(ix, iy, colour);
+                    collisionGrid[ix, iy] = true;
+                } else {
+                    thisBaseTexture.SetPixel(ix, iy, pixel);
+                    collisionGrid[ix, iy] = false;
+                }
             }
         }
-
-        thisBase.Apply();
+        thisBaseTexture.Apply();
 
         Rect rect = new Rect(0, 0, baseTexture.width, baseTexture.height);
-        baseRenderer.sprite = Sprite.Create(thisBase, rect, Vector2.zero);
+        renderer.sprite = Sprite.Create(thisBaseTexture, rect, Vector2.zero);
+
+        Base thisBase = new Base();
+        thisBase.gameObject = baseObj;
+        thisBase.solid = collisionGrid;
+        return thisBase;
     }
-    
-    void Update() {        
+
+    public void hasBombHitBase(GameObject baseObject, GameObject bomb) {
+        for (int ix = 0; ix < bases.Length; ix++) {
+            if (bases[ix].gameObject == baseObject) {
+                GameManager.log("Bomb hit base: " + ix);
+                //
+                // Convert bomb position into base coordinates.
+                // Check if any of that area is solid.
+                // If yes, kill bomb and remove pixels, update solid array.
+                //
+                return;
+            }
+        }
     }
+}
+
+class Base {
+    public GameObject gameObject;
+    public bool[,] solid;
 }
